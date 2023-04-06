772aa41695de3f3d95e387a447b5228c5b8af160
Anastasiia1703 <ostrov270418@gmail.com>
2023-04-06 19:03:16 +0300

    hw js 9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', startButton);
+stopBtn.addEventListener('click', stopButton);
+
+function startButton() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function stopButton() {
+  clearInterval(intervalId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
---
+++ src/js/03-promises.js
@@ -0,0 +1,42 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
+
+  for (let i = 1; i < amount; i += 1) {
+    const position = i;
+    const promiseDelay = delay + (i - 1) * step;
+
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
