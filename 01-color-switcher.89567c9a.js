c8754835379c2dd5caf19a13642ca30fd7c1a308
Anastasiia1703 <111498664+Anastasiia1703@users.noreply.github.com>
2023-04-06 16:35:38 +0000

    Deploying to gh-pages from @ Anastasiia1703/goit-js-hw-09@599321ed091a2aaa5760fa10922f7e149d852ba7 🚀

---
+++ 01-color-switcher.89567c9a.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),a.disabled=!1,e.disabled=!0})),a.addEventListener("click",(function(){clearInterval(t),a.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.89567c9a.js.map
